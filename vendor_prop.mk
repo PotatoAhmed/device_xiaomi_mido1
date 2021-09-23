85d215cf3480508e6ee2fa07a69607690b4295b9
PotatoAhmed <ahmedbasha243@gmail.com>
2021-09-23 17:51:26 +0000

    mido: fix build error

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -176,12 +176,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.max_virtual_display_dimension=4096
 
-# Fling Velocity
-PRODUCT_PROPERTY_OVERRIDES += \ 
-ro.min.fling_velocity=160 \
-ro.max.fling_velocity=20000
-
-
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.fha_enable=true \
