fe49b917480c625b209a95d64d2abc463517569d
PotatoAhmed <ahmedbasha243@gmail.com>
2021-09-23 11:44:03 +0000

    mido: Adjusted Fling Velocity

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -176,6 +176,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.max_virtual_display_dimension=4096
 
+# Fling Velocity
+PRODUCT_PROPERTY_OVERRIDES += \ 
+ro.min.fling_velocity=160
+ro.max.fling_velocity=20000
+
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.delta_time.enable=true
